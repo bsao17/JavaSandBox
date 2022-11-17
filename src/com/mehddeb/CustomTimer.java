bf55e5aac144cea52c077baa61095443713a7ae2
Bruno M <declic62gmail.com>
2022-11-17 07:45:15 +0100

    feat: roll of the dice

--- src/com/mehddeb/CustomTimer.java
+++ src/com/mehddeb/CustomTimer.java
@@ -1,12 +1,5 @@
 package com.mehddeb;
 
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.Soundbank;
-import javax.sound.midi.spi.SoundbankReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.TimerTask;
 
 public class CustomTimer extends TimerTask {
@@ -18,22 +11,6 @@ public class CustomTimer extends TimerTask {
         if (time != 0){
             System.out.println("il reste " + --time + " secondes !");
         } else {
-            SoundbankReader sound = new SoundbankReader() {
-                @Override
-                public Soundbank getSoundbank(URL url) throws InvalidMidiDataException, IOException {
-                    return null;
-                }
-
-                @Override
-                public Soundbank getSoundbank(InputStream stream) throws InvalidMidiDataException, IOException {
-                    return null;
-                }
-
-                @Override
-                public Soundbank getSoundbank(File file) throws InvalidMidiDataException, IOException {
-                    return null;
-                }
-            };
             cancel();
         }
     }
--- src/com/mehddeb/Main.java
+++ src/com/mehddeb/Main.java
@@ -1,39 +1,77 @@
 package com.mehddeb;
 
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Veuillez choisir 1 pour utiliser le Chronomètre et 2 pour la soustraction !");
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+        System.out.println("Bonjour taper 1 pour commencer à jouer et 1 à la fin de chaque partie pour continuer !");
+        byte playing = scanner.nextByte();
+        System.out.println("**** Pour choisir le Chronométre tapez le numéro 1 ****");
+        System.out.println("**** Pour choisir la calculatrice tapez le numéro 2 ****");
+        System.out.println("**** Pour lancer un dè après 2 secondes tapez le numéro 3 ****");
+        System.out.println("**** C'est à vous : ");
 
-        if(choice == 1)
-        {
-            System.out.println("Veuillez choisir la durée du décompte en secondes !");
-            CustomTimer customTimer = new CustomTimer();
-            CustomTimer.time = scanner.nextInt();
-            Timer timer = new Timer();
-            timer.schedule(customTimer, 1000, 1000);
-        }
-        if (choice == 2)
-        {
-            System.out.println("Veuillez choisir les chiffres à soustraire !");
-            substract(scanner.nextInt(), scanner.nextInt(), new CalculListener() {
-                @Override
-                public void onPositiveResult() {
-                    System.out.println("Le résultat est positif !");
-                }
+        while(playing == 1){
 
-                @Override
-                public void onNegativeResult() {
-                    System.out.println("Le résultat est négatif !");
-                }
-            });
+            int choice = scanner.nextInt();
+
+            if(choice == 1)
+            {
+                System.out.println("Veuillez choisir la durée du décompte en secondes !");
+                CustomTimer customTimer = new CustomTimer();
+                CustomTimer.time = scanner.nextInt();
+                Timer timer = new Timer();
+                timer.schedule(customTimer, 1000, 1000);
+            }
+            if (choice == 2)
+            {
+                System.out.println("Veuillez choisir les chiffres à soustraire !");
+                substract(scanner.nextInt(), scanner.nextInt(), new CalculListener() {
+                    @Override
+                    public void onPositiveResult() {
+                        System.out.println("Le résultat est positif !");
+                    }
+
+                    @Override
+                    public void onNegativeResult() {
+                        System.out.println("Le résultat est négatif !");
+                    }
+                });
+            }
+            if(choice == 3) {
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    int time = 2;
+                    @Override
+                    public void run() {
+                        if (time != 0){
+                            System.out.println( time + " secondes ");
+                        } else {
+                            nexInt(1, 6);
+                            cancel();
+                        }
+                        time--;
+                    }
+
+                }, 1000, 1000);
+            }
+            if (choice == 0){
+                System.exit(1);
+            }
         }
+
+
+
+    }
+
+    public static void nexInt(int a, int b){
+        Random random = new Random();
+        System.out.println("Le résultat du lancé de dès est ...");
+        System.out.println(random.nextInt(a, b));
     }
 
     private static void substract(int a, int b, CalculListener calculListener) {
